4c364fcbb0fd8338814074e392bb504e4a342b75
djdapz <djdapz@aol.com>
2017-10-03 20:35:59 -0500

    Created Repo. Chapter 12: addition

---
+++ src/main/kotlin/Money.kt
@@ -0,0 +1,19 @@
+/**
+ * Created by devondapuzzo on 10/3/17.
+ */
+
+open class Money(var amount: Int, var currency: String) {
+    override fun equals(other: Any?): Boolean {
+        val money: Money = other as Money
+        return (money.amount == this.amount && money.currency == this.currency)
+    }
+
+    override fun hashCode(): Int = amount
+
+    operator fun times(multiplier: Int): Money = Money(amount * multiplier, currency)
+
+    companion object {
+        fun dollar(amount: Int): Money = Money(amount, "USD")
+        fun franc(amount: Int): Money = Money(amount, "CHF")
+    }
+}
---
+++ src/test/kotlin/DollarTest.kt
@@ -0,0 +1,39 @@
+import Money.Companion.dollar
+import Money.Companion.franc
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Created by devondapuzzo on 10/2/17.
+ */
+class DollarTest {
+    @Test
+    fun shouldMultiply() {
+        val five: Money = dollar(5)
+        assertThat(dollar(10)).isEqualTo(five * 2)
+        assertThat(dollar(15)).isEqualTo(five * 3)
+    }
+
+    @Test
+    fun shouldMultiplyFrancs() {
+        val five: Money = franc(5)
+        assertThat(franc(10)).isEqualTo(five * 2)
+        assertThat(franc(15)).isEqualTo(five * 3)
+    }
+
+
+    @Test
+    fun shouldBeAbleToAssertEquality() {
+        assertTrue(dollar(5) == (dollar(5)))
+        assertFalse(dollar(5) == (dollar(6)))
+        assertFalse(dollar(5) == (franc(5)))
+    }
+
+    @Test
+    fun testCurrency() {
+        assertThat("USD").isEqualTo(dollar(1).currency)
+        assertThat("CHF").isEqualTo(franc(1).currency)
+    }
+}
