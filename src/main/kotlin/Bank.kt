850bb13601c36a1e9315323daf20e4d0eefe1988
djdapz <djdapz@aol.com>
2017-10-04 19:49:32 -0500

    #14 - Introduce FX Rates

--- src/main/kotlin/Bank.kt
+++ src/main/kotlin/Bank.kt
@@ -1,9 +1,23 @@
+import java.util.*
+
 /**
  * Created by devondapuzzo on 10/3/17.
  */
 
 class Bank {
     companion object{
-        fun reduce(source: Expression, to: String): Money = source.reduce(to)
+        private var rates = Hashtable<Pair, Int>()
+        fun reduceBank(source: Expression, to: String): Money = source.reduce(to)
+        fun addRate(from: String, to: String, rate: Int) {
+            rates.put(Pair(from, to), rate)
+        }
+
+        fun getRate(from: String, to: String) : Int? {
+            if(from == to){
+                return 1
+            }else{
+                return rates[Pair(from ,to)]
+            }
+        }
     }
 }
